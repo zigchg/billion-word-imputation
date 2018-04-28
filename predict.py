ba541d929155b59094464cbff2a61b6380794bf3
Chong Zhang <zigchg@gmail.com>
2018-04-27 21:14:08 -0400

    train whole dataset

--- predict.py
+++ predict.py
@@ -1,24 +1,44 @@
 from stanfordcorenlp import StanfordCoreNLP
 import json
 from collections import Counter
+from math import log
 
 UNK = "<UNKNOWN>"
 
 nlp = StanfordCoreNLP(r"./stanford-corenlp-full-2018-02-27")
 allsentences = []
 
+tag2tagweight_tag = .5
+word2wordweight_tag = 1
+
+tag2tagweight_word = 1
+word2wordweight_word = .5
+
 allTagCounts = Counter(json.loads(open("allTagCounts.json","r").readline()))
 emissionDists = Counter(json.loads(open("emissionDists.json","r").readline()))
 perWordTagCounts = Counter(json.loads(open("perWordTagCounts.json","r").readline()))
 transitionDists = Counter(json.loads(open("transitionDists.json","r").readline()))
+word2wordDists = Counter(json.loads(open("word2word.json","r").readline()))
+
+v_size = len(word2wordDists)
+alpha = 0.1
+
+unk2any = log((1/v_size+alpha)/(v_size+alpha*v_size))
 
 cnt = 0
 
 for tag in emissionDists:
     emissionDists[tag] = Counter(emissionDists[tag])
 
+lineCounter = 0
+    
 with open("test_v2.txt","r") as f:
+    
+    
     for line in f:
+        lineCounter+=1
+        if lineCounter>100:
+            break
         
         thisid, sentence = line.split(",",1)
         if thisid=="id":
@@ -35,27 +55,43 @@ with open("test_v2.txt","r") as f:
         previousword = tagged[0][0]
         previoustag = tagged[0][1]
         
+        thisprob = 0
         
         for idx, each_pair in enumerate(tagged):
             word = each_pair[0]
             tag = each_pair[1]
             
-            if idx==0:
+            if idx==0 or idx==len(tagged)-1:
                 continue
                 
             if tag in transitionDists[previoustag]:
-                thisprob = transitionDists[previoustag][tag] # * emissionDists[tag][word]
+                thisprob = tag2tagweight_tag * transitionDists[previoustag][tag] # * emissionDists[tag][word]
             else:
-                thisprob = transitionDists[previoustag][UNK]
+                thisprob = tag2tagweight_tag * transitionDists[previoustag][UNK]
+            
+            if previousword not in word2wordDists:
+                thisprob += unk2any
+            elif word in word2wordDists[previousword]:
+                thisprob += word2wordweight_tag * word2wordDists[previousword][word]
+            else:
+                thisprob += word2wordweight_tag * word2wordDists[previousword][UNK]
             
             if thisprob<leastprob:
                 leastprob = thisprob
                 missingwordposition = idx
+            
+            previousword = word
+            previoustag = tag
                 
         previoustag = tagged[missingwordposition-1][1]
         nexttag = tagged[missingwordposition][1]
+        
+        previousword = tagged[missingwordposition-1][0]
+        nextword = tagged[missingwordposition][0]
+        
         missingtag = ''
         mostprob = float("-inf")
+        thisprob = 0
         
         for tag in emissionDists:
             if tag in transitionDists[previoustag]:
@@ -72,14 +108,45 @@ with open("test_v2.txt","r") as f:
                 mostprob = thisprob
                 missingtag = tag
                 
-        missingword = emissionDists[missingtag].most_common(1)[0][0]
+        # missingword = emissionDists[missingtag].most_common(1)[0][0]
+        
+        mostprob = float("-inf")
+        thisprob = 0
+        missingword = UNK
+        
+        for possibleword in emissionDists[missingtag]:
+            if possibleword==UNK:
+                continue
+            
+            if possibleword in emissionDists[missingtag]:
+                thisprob = tag2tagweight_word * emissionDists[missingtag][possibleword]
+            else:
+                thisprob = tag2tagweight_word * emissionDists[missingtag][UNK]
+            
+            if previousword not in word2wordDists:
+                thisprob+= unk2any
+            elif possibleword in word2wordDists[previousword]:
+                thisprob+=word2wordweight_word * word2wordDists[previousword][possibleword]
+            else:
+                thisprob+=word2wordweight_word * word2wordDists[previousword][UNK]
+                
+            if possibleword not in word2wordDists:
+                thisprob += unk2any
+            elif nextword in word2wordDists[possibleword]:
+                thisprob += word2wordweight_word * word2wordDists[possibleword][nextword]
+            else:
+                thisprob += word2wordweight_word * word2wordDists[possibleword][UNK]
+            
+            if thisprob>mostprob:
+                mostprob = thisprob
+                missingword = possibleword
         
         splitsentence = sentence.split(" ")
         fullsentence = splitsentence[0:missingwordposition] + ["("+missingword+")"] + splitsentence[missingwordposition:] 
         allsentences.append(" ".join(fullsentence))
         
         cnt+=1
-        if cnt%1000==0:
+        if cnt%100==0:
             print(cnt)
             print(" ".join(fullsentence))
         
--- training.py
+++ training.py
@@ -14,20 +14,30 @@ emissionCounts = {}
 # log probability distributions: do NOT use Counters inside these because missing Counter entries default to 0, not log(0)
 transitionDists = {}
 emissionDists = {}
+word2word = {}
+word2wordDists = {}
+#tag2tag = {}
+#tag2tagDists = {}
 
 UNK = "<UNKNOWN>"
+
+lineCount = 0
 
 # read in statistics
 with open("train_v2.txt") as f:
     for line in f:
-        try:
-            tagged_sentence = nlp.pos_tag(line)
-        except:
-            print(line)
-            continue
+        lineCount += 1
+#        if lineCount > 2000:
+#            break
+        
+        if lineCount%10000==0:
+            print(lineCount)
+        
+        tagged_sentence = nlp.pos_tag(line)
         previousTag = ""
+        previousWord = ""
         for idx, each_pair in enumerate(tagged_sentence):
-            word = each_pair[0]
+            word = each_pair[0].lower()
             tag = each_pair[1]
             if tag not in allTagCounts:
                 allTagCounts[tag] = 0
@@ -40,14 +50,29 @@ with open("train_v2.txt") as f:
                 perWordTagCounts[word][tag] = 0
             perWordTagCounts[word][tag] += 1
             
-            if idx!=1:
+            if idx!=0:
                 if previousTag not in transitionCounts:
                     transitionCounts[previousTag] = Counter()
                     transitionCounts[previousTag][tag] = 0
                 elif tag not in transitionCounts[previousTag]:
                     transitionCounts[previousTag][tag] = 0
                 transitionCounts[previousTag][tag] += 1
-            
+                
+                if previousWord not in word2word:
+                    word2word[previousWord] = {}
+                    word2word[previousWord][word] = 0
+                elif word not in word2word[previousWord]:
+                    word2word[previousWord][word] = 0
+                word2word[previousWord][word] += 1
+                    
+#                if previousTag not in tag2tag:
+#                    tag2tag[previousTag] = {}
+#                    tag2tag[previousTag][tag] = 0
+#                elif tag not in tag2tag[previousTag]:
+#                    tag2tag[previousTag][tag] = 0
+#                tag2tag[previousTag][tag] += 1    
+                    
+            previousWord = word
             previousTag = tag
             
             if tag not in emissionCounts:
@@ -56,9 +81,11 @@ with open("train_v2.txt") as f:
             elif word not in emissionCounts[tag]:
                 emissionCounts[tag][word] = 0
             emissionCounts[tag][word] += 1
-        
+                
     # normalize counts and store log probability distributions in transitionDists and emissionDists
     # ...
+    
+    
     for given_tag in transitionCounts:
         transitionCounts[given_tag][UNK] = 0
         total = sum(transitionCounts[given_tag].values(),0.0)
@@ -74,17 +101,39 @@ with open("train_v2.txt") as f:
         emissionDists[given_tag] = {}
         for word in emissionCounts[given_tag]:
             emissionDists[given_tag][word] = log((emissionCounts[given_tag][word]+ALPHA)/(total+ALPHA*length))
+            
+    for given_word in word2word:
+        word2word[given_word][UNK] = 0
+        total = sum(word2word[given_word].values(),0.0)
+        length = len(word2word[given_word])
+        word2wordDists[given_word] = {}
+        for word in word2word[given_word]:
+            word2wordDists[given_word][word] = log((word2word[given_word][word]+ALPHA)/(total+ALPHA*length))
+            
+#    for given_tag in tag2tag:
+#        tag2tag[given_tag][UNK] = 0
+#        total = sum(tag2tag[given_tag].values(),0.0)
+#        length = len(tag2tag[given_tag])
+#        tag2tagDists[given_tag] = {}
+#        for tag in tag2tag[given_tag]:
+#            tag2tagDists[given_tag][tag] = log((tag2tag[given_tag][tag]+ALPHA)/(total+ALPHA*length))
 
 nlp.close()
 
-with open("allTagCounts.json") as f:
+with open("allTagCounts.json","w") as f:
     f.write(json.dumps(allTagCounts))
 
-with open("perWordTagCounts.json") as f:
+with open("perWordTagCounts.json","w") as f:
     f.write(json.dumps(perWordTagCounts))
     
-with open("transitionDists.json") as f:
+with open("transitionDists.json","w") as f:
     f.write(json.dumps(transitionDists))
     
-with open("emissionDists.json") as f:
+with open("emissionDists.json","w") as f:
     f.write(json.dumps(emissionDists))
+    
+with open("word2word.json","w") as f:
+    f.write(json.dumps(word2wordDists))
+
+#with open("tag2tag.json","w") as f:
+#    f.write(json.dumps(tag2tagDists))
